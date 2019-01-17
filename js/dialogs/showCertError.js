8b0ec2956eaf501436f9479d5668de7bc3ddd86e
Kiran Niranjan <kiran.niranjan@symphony.com>
2019-01-17 09:42:18 +0530

    Allow all certs

--- js/dialogs/showCertError.js
+++ js/dialogs/showCertError.js
@@ -4,9 +4,6 @@ const electron = require('electron');
 
 const log = require('../log.js');
 const logLevels = require('../enums/logLevels.js');
-const i18n = require('../translation/i18n');
-
-let ignoreAllCertErrors = true;
 
 /**
  * If certificate error occurs allow user to deny or allow particular certificate
@@ -18,31 +15,7 @@ let ignoreAllCertErrors = true;
  */
 electron.app.on('certificate-error', function(event, webContents, url, error,
     certificate, callback) {
-
-    if (ignoreAllCertErrors) {
-        event.preventDefault();
-        callback(true);
-        return;
-    }
-
-    log.send(logLevels.WARNING, 'Certificate error: ' + error + ' for url: ' + url);
-
-    const browserWin = electron.BrowserWindow.fromWebContents(webContents);
-    const buttonId = electron.dialog.showMessageBox(browserWin, {
-        type: 'warning',
-        buttons: [ 'Allow', 'Deny', 'Ignore All' ],
-        defaultId: 1,
-        cancelId: 1,
-        noLink: true,
-        title: i18n.getMessageFor('Certificate Error'),
-        message: i18n.getMessageFor('Certificate Error') + `: ${error}\nURL: ${url}`,
-    });
-
+    log.send(logLevels.INFO, 'Certificate error: ' + error + ' for url: ' + url);
     event.preventDefault();
-
-    if (buttonId === 2) {
-        ignoreAllCertErrors = true;
-    }
-
-    callback(buttonId !== 1);
+    callback(true);
 });
